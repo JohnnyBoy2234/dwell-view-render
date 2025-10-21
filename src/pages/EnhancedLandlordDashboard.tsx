94e89b6c1dd58692174bb20550f7d811c28c580f
JohnnyBoy2234 <jonathantheron34@gmail.com>
2025-10-21 22:11:45 +0200

    dash

--- src/pages/EnhancedLandlordDashboard.tsx
+++ src/pages/EnhancedLandlordDashboard.tsx
@@ -1432,7 +1432,7 @@ export default function EnhancedLandlordDashboard() {
       </div>
     );
   };
-
+  
   const renderPropertiesTab = () => (
     <div className="space-y-6">
       <div className="flex items-center justify-between mb-6">
