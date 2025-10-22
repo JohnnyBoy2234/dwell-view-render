aa60254200b3d5ebca6913e7af2c0a62082fafe5
Codeable1 <cjtheron2019@gmail.com>
2025-10-22 11:32:35 +0200

    changed send-email edge function

--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -7,9 +7,6 @@ import {createClient} from "https://esm.sh/@supabase/supabase-js@2";
 serve(async (res: Request) => {
     try{
         const resend = new Resend(Deno.env.get("RESEND_API_KEY")!);
-        const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
-        const supabaseServiceRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
-        const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 
         const {to, subject, html} = await res.json();
 
