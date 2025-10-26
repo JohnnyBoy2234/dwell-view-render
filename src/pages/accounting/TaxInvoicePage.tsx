718f00aa1f918dc6d7dd97b5ea087cbf7f84bcda
JohnnyBoy2234 <jonathantheron34@gmail.com>
2025-10-26 13:55:07 +0200

    back button

--- src/pages/accounting/TaxInvoicePage.tsx
+++ src/pages/accounting/TaxInvoicePage.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 export default function TaxInvoicePage() {
   const navigate = useNavigate();
   
+  // This will be used by the back button in the header
   const handleBack = () => {
     navigate('/enhancedlandlorddashboard');
   };
@@ -13,8 +14,17 @@ export default function TaxInvoicePage() {
   return (
     <EnhancedDashboardLayout 
       title="Generate Tax Invoice"
-      currentTab="/tax-invoice"
-      onTabChange={handleBack}
+      currentTab="/accounting" // This should match the tab you want to be active in the sidebar
+      onTabChange={(tab) => {
+        // Only navigate if trying to change to a different tab
+        if (tab !== '/accounting') {
+          navigate(tab);
+        }
+      }}
+      // Force the back button to always go to management tools
+      onBackToProperties={handleBack}
+      // This will make the back button always visible
+      selectedPropertyId="dummy"
     >
       <TaxInvoiceGenerator />
     </EnhancedDashboardLayout>
