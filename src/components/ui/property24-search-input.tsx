becc0f0b3caa211cc066cbf37dd928399ff0f7cf
Codeable1 <cjtheron2019@gmail.com>
2026-01-02 18:08:02 +0200

    .

--- src/components/ui/property24-search-input.tsx
+++ src/components/ui/property24-search-input.tsx
@@ -284,7 +284,7 @@ export const Property24SearchInput: React.FC<Property24SearchInputProps> = ({
   }, []);
 
   return (
-    <div className="relative w-full">
+    <div className="relative w-full" style={{isolation: 'isolate'}}>
       {/* Search Input */}
       <div className="relative group">
         <MapPin className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground z-10 pointer-events-none" />
