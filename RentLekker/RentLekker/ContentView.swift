686e317129ad6a2f689270e8a280e0dd175d9ddd
JohnnyBoy2234 <jonathantheron34@gmail.com>
2026-01-14 09:50:12 +0200

    Added ios files

---
+++ RentLekker/RentLekker/ContentView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+}
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://rentlekker.com")!)
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
